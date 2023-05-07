3d041dd972dd6e3b1f6d67e5f9d9fa17bdb76876
yara <89746218+yaramostafa@users.noreply.github.com>
2023-05-07 18:17:32 +0300

    K-Medoids added

--- algorithms.py
+++ algorithms.py
@@ -1,24 +1,326 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve,  auc
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 #---------------------------------------------Clustering----------------------------------------------
+#          k mediod
+def kmediods(X_train, X_test, y_train, y_test):
+
+    kmedoids = KMedoids(n_clusters=2).fit(X_train)
+
+    # Get labels for each point in training data
+    y_predict = np.array(kmedoids.labels_)
+
+    # View final mediods
+    print(kmedoids.cluster_centers_)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_flipped = np.where((y_predict == 0) | (y_predict == 1), y_predict ^ 1, y_predict)
+
+    # Test the model on the test data
+    y_predict_test = KMedoids.predict(kmedoids, X_test)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_test_flipped = np.where((y_predict_test == 0) | (y_predict_test == 1), y_predict_test ^ 1, y_predict_test)
+
+    # Evaluate the performance of the model on both training and testing data
+    train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+    test_acc = max(accuracy_score(y_test, y_predict_test), accuracy_score(y_test, y_predict_test_flipped))
+
+    print("Training data labels: ")
+    print(kmedoids.labels_)
+    print("---------------------------------------------------------------------")
+    print("Cluster Centers")
+    print(kmedoids.cluster_centers_)
+    print("---------------------------------------------------------------------")
+    print("Training Accuracy: ", "{:.4f}".format(train_acc * 100), "%")
+    print("Testing Accuracy: ", "{:.4f}".format(test_acc * 100), "%")
+
+    # Reduce training data to 2 features using pca to plot results
+    pca = PCA(n_components=2)
+
+    X_train_plt = pca.fit_transform(X_train)
+
+    kmediods_plt = KMedoids(n_clusters=2).fit(X_train_plt)
+
+    # Generate a scatter plot graph for 2 clusters and their centers
+    plt.scatter(X_train_plt[:, 0][kmediods_plt.labels_ == 0], X_train_plt[:, 1][kmediods_plt.labels_ == 0], s=3, c='r')
+    plt.scatter(X_train_plt[:, 0][kmediods_plt.labels_ == 1], X_train_plt[:, 1][kmediods_plt.labels_ == 1], s=3, c='b')
+    plt.plot(kmediods_plt .cluster_centers_[0][0], kmediods_plt.cluster_centers_[0][1], marker="x", markersize=10,
+             markeredgecolor="black")
+    plt.plot(kmediods_plt .cluster_centers_[1][0], kmediods_plt.cluster_centers_[1][1], marker="x", markersize=10,
+             markeredgecolor="black")
+
+    plt.show()
+
+    # Convert class labels from integer to string format
+    y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
+    if accuracy_score(y_test, y_predict_test) > accuracy_score(y_test, y_predict_test_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test_flipped]
+
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
+    plt.tight_layout()
+    plt.show()
 
 #           kmeans - kmediod
 
+
+def kmeans(X_train, X_test, y_train, y_test, n_clusters=2):
+    # Create a kmeans clustering model and fit on the training data
+    kmeans = KMeans(n_clusters=n_clusters).fit(X_train)
+
+    # Get labels for each point in training data
+    y_predict = np.array(kmeans.labels_)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_flipped = np.where((y_predict==0)|(y_predict==1), y_predict^1, y_predict)
+
+    # Test the model on the test data
+    y_predict_test = KMeans.predict(kmeans, X_test)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_test_flipped = np.where((y_predict_test==0)|(y_predict_test==1), y_predict_test^1, y_predict_test)
+
+    # Evaluate the performance of the model on both training and testing data
+    train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+    test_acc = max(accuracy_score(y_test, y_predict_test),accuracy_score(y_test, y_predict_test_flipped))
+
+    print("Training data labels: ")
+    print(kmeans.labels_)
+    print("---------------------------------------------------------------------")
+    print("Cluster Centers")
+    print(kmeans.cluster_centers_)
+    print("---------------------------------------------------------------------")
+    print("Training Accuracy: ", "{:.4f}".format(train_acc*100), "%")
+    print("Testing Accuracy: ", "{:.4f}".format(test_acc*100), "%")
+
+    # Reduce training data to 2 features using pca to plot results
+    pca = PCA(n_components=2)
+
+    X_train_plt = pca.fit_transform(X_train)
+
+    kmeans_plt = KMeans(n_clusters=n_clusters).fit(X_train_plt)
+
+    # Generate a scatter plot graph for 2 clusters and their centers
+    plt.scatter(X_train_plt[:, 0][kmeans_plt.labels_ == 0], X_train_plt[:, 1][kmeans_plt.labels_ == 0], s=3, c='r')
+    plt.scatter(X_train_plt[:, 0][kmeans_plt.labels_ == 1], X_train_plt[:, 1][kmeans_plt.labels_ == 1], s=3, c='b')
+    plt.plot(kmeans_plt.cluster_centers_[0][0], kmeans_plt.cluster_centers_[0][1], marker="x", markersize=10, markeredgecolor="black")
+    plt.plot(kmeans_plt.cluster_centers_[1][0], kmeans_plt.cluster_centers_[1][1], marker="x", markersize=10, markeredgecolor="black")
+
+    plt.show()
+
+    # Convert class labels from integer to string format
+    y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
+    if accuracy_score(y_test, y_predict_test) > accuracy_score(y_test, y_predict_test_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test_flipped]
+
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
 #           DBscan
+
+
+def DBscan(X_train, X_test, y_train, y_test):
+    acc = []
+    for i in range(1, 10):
+        # Create a dbscan clustering model and fit on the training data with different maximum distance
+        dbscan = DBSCAN(eps=(i/2), min_samples=3).fit(X_train)
+
+        y_predict = np.array(dbscan.labels_)
+        y_predict_flipped = np.where((y_predict == 0) | (y_predict == 1), y_predict ^ 1, y_predict)
+        train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+
+        acc.append(train_acc)
+
+    ep_highest = (acc.index(max(acc)) + 1)/2
+
+    # Create a dbscan clustering model and fit on the training data with different maximum distance
+    highest_dbscan = DBSCAN(eps=ep_highest+0.5, min_samples=3).fit(X_train)  # highest ep shows overfitting so we'll take second highest value which mostly solves this issue
+
+    # Get labels for each point in training data
+    y_predict = highest_dbscan.labels_
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_flipped = np.where((y_predict == 0) | (y_predict == 1), y_predict ^ 1, y_predict)
+
+    # Evaluate the performance of the model on training data
+    train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+
+    print("Training data labels: ")
+    print(highest_dbscan.labels_)
+    print("---------------------------------------------------------------------")
+
+    # Test the model on the test data
+    y_predict_test = DBSCAN.fit_predict(highest_dbscan, X_test)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_test_flipped = np.where((y_predict_test == 0) | (y_predict_test == 1), y_predict_test ^ 1, y_predict_test)
+
+    # Evaluate the performance of the model on testing data
+    test_acc = max(accuracy_score(y_test, y_predict_test), accuracy_score(y_test, y_predict_test_flipped))
+
+    print("Test data labels: ")
+    print(highest_dbscan.labels_)
+    print("---------------------------------------------------------------------")
+    print("Training Accuracy: ", "{:.4f}".format(train_acc*100), "%")
+    print("Testing Accuracy: ", "{:.4f}".format(test_acc * 100), "%")
+
+    # Convert class labels from integer to string format
+    y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
+    if accuracy_score(y_test, y_predict_test) > accuracy_score(y_test, y_predict_test_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test_flipped]
+
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+
+
 
 #           Hierarchical clustering
 
-#           Gaussian Mixture Models (NEW)
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+def agglomerative_clustering(X_train, X_test, y_train, y_test):
+    # Create agglomerative clustering model and fit on the training data
+    Agglomerative = AgglomerativeClustering().fit(X_train)
+
+    # Create agglomerative clustering model and fit on the training data to plot dendrogram
+    Agglomerative_plt = AgglomerativeClustering(distance_threshold=0, n_clusters=None).fit(X_train)
+
+    # Get labels for each point in training data
+    y_predict = Agglomerative.labels_
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_flipped = np.where((y_predict == 0) | (y_predict == 1), y_predict ^ 1, y_predict)
+
+    # Evaluate the performance of the model on training data
+    train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+
+    print("Training data labels: ")
+    print(Agglomerative.labels_)
+    print("---------------------------------------------------------------------")
+
+    # Test the model on the test data
+    y_predict_test = AgglomerativeClustering().fit_predict(X_test)
+
+    # Flip the 0s to 1s and vice versa
+    y_predict_test_flipped = np.where((y_predict_test == 0) | (y_predict_test == 1), y_predict_test ^ 1, y_predict_test)
+
+    # Evaluate the performance of the model on testing data
+    test_acc = max(accuracy_score(y_test, y_predict_test), accuracy_score(y_test, y_predict_test_flipped))
+
+    print("Test data labels: ")
+    print(y_predict_test)
+    print("---------------------------------------------------------------------")
+    print("Training Accuracy: ", "{:.4f}".format(train_acc*100), "%")
+    print("Testing Accuracy: ", "{:.4f}".format(test_acc*100), "%")
+
+    plt.title("Hierarchical Clustering Dendrogram")
+    # plot the top three levels of the dendrogram
+    plot_dendrogram(Agglomerative_plt, truncate_mode="level", p=3)
+    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    plt.show()
+
+    # Convert class labels from integer to string format
+    y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
+    if accuracy_score(y_test, y_predict_test) > accuracy_score(y_test, y_predict_test_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test_flipped]
+
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
 
 #-------------------------------------------Classification---------------------------------------------
 
@@ -90,13 +392,12 @@ def KNN(X_train, X_test, y_train, y_test):
     plt.scatter(X_test_plt[y_pred_plot == 1], y_pred_plot[y_pred_plot == 1], s=3, c='b')
     plt.show()
 
-
-
 #           Support Vector Machines (SVM)
 
 #           Naive Bayes
 
 #           Decision Trees
+
 
 
 def decision_tree(X_train, X_test, y_train, y_test):
@@ -123,16 +424,24 @@ def LinearReg(X_train, X_test, y_train, y_test):
     print(f'regression score : {regr.score(X_test, y_test)}')
 
     y_pred = regr.predict(X_test)
+
+    y_pred = np.where(y_pred > 0.5, 1, 0)
+
+    y_pred_flipped = np.where((y_pred == 0) | (y_pred == 1), y_pred ^ 1, y_pred)
+
     mae = mean_absolute_error(y_test, y_pred)
     mse = mean_squared_error(y_test, y_pred)
     rmse = np.sqrt(mse)
-    print(f'Mean absolute error: {mae:.2f}')
-    print(f'Mean squared error: {mse:.2f}')
-    print(f'Root mean squared error: {rmse:.2f}')
+    print(f'Mean absolute error: {mae:.4f}')
+    print(f'Mean squared error: {mse:.4f}')
+    print(f'Root mean squared error: {rmse:.4f}')
 
     # Convert class labels from integer to string format
     y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
-    y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_pred]
+    if accuracy_score(y_test, y_pred) > accuracy_score(y_test, y_pred_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_pred]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_pred_flipped]
 
     # Evaluate the performance of the classifier
     print("Accuracy:", accuracy_score(y_true_str, y_pred_str)*100)
@@ -170,7 +479,6 @@ def RandomForest(X_train, X_test, y_train, y_test):
     y_pred = rf.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
-
 
 #           Logistic Regression (NEW)
 
@@ -236,13 +544,13 @@ def logistic_regression(X_train, X_test, y_train, y_test):
 
 #           Scatterplots
 
-#           Dendrogram
+#           Dendrogram **
 
-#           Confusion Matrix (NEW)
+#           Confusion Matrix (NEW) **
 
-#           Receiver Operating Characteristic (ROC) curve (NEW)
+#           Receiver Operating Characteristic (ROC) curve (NEW) **
 
-#           Heatmap (NEW)
+#           Heatmap (NEW) **
 
 #           Silhouette Plot (NEW)
 
