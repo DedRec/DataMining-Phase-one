72a88dfaca5c853cc1a3a70da2599607b279925e
DedRec <89926533+DedRec@users.noreply.github.com>
2023-05-09 23:48:52 +0300

    Final algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,19 +1,26 @@
+from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from scipy.cluster.hierarchy import dendrogram
-from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.tree import plot_tree, DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc, precision_recall_curve, \
     mean_absolute_error, mean_squared_error
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import plot_tree, DecisionTreeClassifier
-from sklearn import svm
-from Project import *
+
+from preprocessing import *
 import numpy as np
 from sklearn.calibration import CalibratedClassifierCV, calibration_curve
 from sklearn.metrics import brier_score_loss
 
-show_graphs = False
+show_graphs = True
 # Read in the CSV file
 df = pd.read_csv('breast-cancer.csv')
 
@@ -23,8 +30,74 @@ labels.pop(0)
 labels.pop(0)
 
 #---------------------------------------------Clustering----------------------------------------------
+#          k mediod
+def kmediods(X_train, X_test, y_train, y_test):
+    print("#---------------------------------------------k mediod----------------------------------------------")
+    kmedoids = KMedoids(n_clusters=2).fit(X_train)
+    # Get labels for each point in training data
+    y_predict = np.array(kmedoids.labels_)
+    # View final mediods
+    print(kmedoids.cluster_centers_)
+    # Flip the 0s to 1s and vice versa
+    y_predict_flipped = np.where((y_predict == 0) | (y_predict == 1), y_predict ^ 1, y_predict)
+    # Test the model on the test data
+    y_predict_test = KMedoids.predict(kmedoids, X_test)
+    # Flip the 0s to 1s and vice versa
+    y_predict_test_flipped = np.where((y_predict_test == 0) | (y_predict_test == 1), y_predict_test ^ 1, y_predict_test)
 
-#           kmeans - kmediod
+    # Evaluate the performance of the model on both training and testing data
+    train_acc = max(accuracy_score(y_train, y_predict), accuracy_score(y_train, y_predict_flipped))
+    test_acc = max(accuracy_score(y_test, y_predict_test), accuracy_score(y_test, y_predict_test_flipped))
+
+    print("Training data labels: ")
+    print(kmedoids.labels_)
+    print("---------------------------------------------------------------------")
+    print("Cluster Centers")
+    print(kmedoids.cluster_centers_)
+    print("---------------------------------------------------------------------")
+    print("Training Accuracy: ", "{:.4f}".format(train_acc * 100), "%")
+    print("Testing Accuracy: ", "{:.4f}".format(test_acc * 100), "%")
+
+    # Reduce training data to 2 features using pca to plot results
+    pca = PCA(n_components=2)
+
+    X_train_plt = pca.fit_transform(X_train)
+
+    kmediods_plt = KMedoids(n_clusters=2).fit(X_train_plt)
+
+    # Generate a scatter plot graph for 2 clusters and their centers
+    plt.scatter(X_train_plt[:, 0][kmediods_plt.labels_ == 0], X_train_plt[:, 1][kmediods_plt.labels_ == 0], s=3, c='r')
+    plt.scatter(X_train_plt[:, 0][kmediods_plt.labels_ == 1], X_train_plt[:, 1][kmediods_plt.labels_ == 1], s=3, c='b')
+    plt.plot(kmediods_plt .cluster_centers_[0][0], kmediods_plt.cluster_centers_[0][1], marker="x", markersize=10,
+             markeredgecolor="black")
+    plt.plot(kmediods_plt .cluster_centers_[1][0], kmediods_plt.cluster_centers_[1][1], marker="x", markersize=10,
+             markeredgecolor="black")
+
+    plt.show()
+
+    # Convert class labels from integer to string format
+    y_true_str = ["Benign" if label == 0 else "Malignant" for label in y_test]
+    if accuracy_score(y_test, y_predict_test) > accuracy_score(y_test, y_predict_test_flipped):
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test]
+    else:
+        y_pred_str = ["Benign" if label == 0 else "Malignant" for label in y_predict_test_flipped]
+
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+
+#           kmeans
 def kmeans(X_train, X_test, y_train, y_test, n_clusters=2):
     print("#---------------------------------------------kmeans----------------------------------------------")
 
@@ -263,7 +336,7 @@ def KNN(X_train, X_test, y_train, y_test):
     KNN_score = []
 
     # Looping over different values of K
-    for i in range(2, 10):
+    for i in range(2, 11):
         KNN = KNeighborsClassifier(n_neighbors=i)
         KNN.fit(X_train, y_train)
         KNN_score.append(KNN.score(X_train, y_train))
@@ -332,8 +405,6 @@ def KNN(X_train, X_test, y_train, y_test):
 #           Decision Trees
 def decision_tree(X_train, X_test, y_train, y_test):
     print("#---------------------------------------------Decision_tree----------------------------------------------")
-
-
     model = DecisionTreeClassifier(random_state=0)
 
     # Train the classifier on the training data
@@ -405,41 +476,55 @@ def LinearReg(X_train, X_test, y_train, y_test):
     # Evaluate the performance of the classifier
     print("Accuracy:", accuracy_score(y_true_str, y_pred_str)*100)
 
-    if (show_graphs):
-        # Generate a confusion matrix plot
-        cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
-        cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
-        fig, ax = plt.subplots(figsize=(5, 4))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
-                    cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
-        cbar = ax.figure.colorbar(ax.collections[0])
-        cbar.ax.tick_params(labelsize=12)
-        plt.title('Confusion Matrix', fontsize=14)
-        plt.xlabel('Predicted label', fontsize=12)
-        plt.ylabel('True label', fontsize=12)
-        plt.tight_layout()
-        plt.show()
+    # Generate a confusion matrix plot
+    cm = confusion_matrix(y_true_str, y_pred_str, labels=["Benign", "Malignant"])
+    cm_labels = {"Benign": "Benign", "Malignant": "Malignant"}
+    fig, ax = plt.subplots(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray')
+    cbar = ax.figure.colorbar(ax.collections[0])
+    cbar.ax.tick_params(labelsize=12)
+    plt.title('Confusion Matrix', fontsize=14)
+    plt.xlabel('Predicted label', fontsize=12)
+    plt.ylabel('True label', fontsize=12)
 
-        # Generate a feature importance plot (for models with coefficients available)
-        if hasattr(regr, 'coef_'):
-            coefs = regr.coef_.ravel()
-            if len(coefs) == 5:
-                names = ['symmetry_se', 'smoothness_mean', 'texture_se', 'symmetry_worst', 'compactness_se']
-            else:
-                names = labels
-            fig3, ax3 = plt.subplots(figsize=(7, 5))
-            ax3.barh(names, coefs, height=0.7, color=plt.cm.RdBu(np.sign(coefs)))
-            ax3.set_yticks(names)
-            ax3.set_xlabel('Coefficient', fontsize=12)
-            ax3.set_ylabel('Feature', fontsize=12)
-            ax3.set_title('Feature Importance', fontsize=14)
-            fig, ax = plt.subplots(figsize=(7, 5))
-            ax.barh(names, coefs, height=0.7, color=plt.cm.RdBu(np.sign(coefs)))
-            ax.set_yticks(names)
-            ax.set_xlabel('Coefficient', fontsize=12)
-            ax.set_ylabel('Feature', fontsize=12)
-            ax.set_title('Feature Importance', fontsize=14)
-            plt.show()
+    # Generate an ROC curve plot
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    roc_auc = auc(fpr, tpr)
+
+    # Generate a feature importance plot (for models with coefficients available)
+    if hasattr(regr, 'coef_'):
+        coefs = regr.coef_.ravel()
+        if len(coefs) == 5:
+            names = ['symmetry_se', 'smoothness_mean', 'texture_se', 'symmetry_worst', 'compactness_se']
+        else:
+            names = labels
+        fig3, ax3 = plt.subplots(figsize=(7, 5))
+        ax3.barh(names, coefs, height=0.7, color=plt.cm.RdBu(np.sign(coefs)))
+        ax3.set_yticks(names)
+        ax3.set_xlabel('Coefficient', fontsize=12)
+        ax3.set_ylabel('Feature', fontsize=12)
+        ax3.set_title('Feature Importance', fontsize=14)
+
+    # Show all plots
+    fig2, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=cm_labels.values(), yticklabels=cm_labels.values(),
+                cbar=False, annot_kws={"fontsize": 12}, linewidths=.5, linecolor='lightgray', ax=ax1)
+    ax1.set_title('Confusion Matrix', fontsize=14)
+    ax1.set_xlabel('Predicted label', fontsize=12)
+    ax1.set_ylabel('True label', fontsize=12)
+    ax2.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    ax2.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    ax2.set_xlim([0.0, 1.0])
+    ax2.set_ylim([0.0, 1.05])
+    ax2.set_xlabel('False Positive Rate')
+    ax2.set_ylabel('True Positive Rate')
+    ax2.set_title('Receiver Operating Characteristic (ROC) Curve')
+    ax2.legend(loc="lower right")
+    plt.show()
+
+
 
 #           Random Forests (NEW)
 def RandomForest(X_train, X_test, y_train, y_test):
@@ -450,6 +535,7 @@ def RandomForest(X_train, X_test, y_train, y_test):
     y_pred = rf.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
+
 
 #           Support Vector Machines (SVM)
 def support_vector_machines(X_train, X_test, y_train, y_test):
@@ -678,3 +764,5 @@ def logistic_regression(X_train, X_test, y_train, y_test):
         ax2.set_title('Receiver Operating Characteristic (ROC) Curve')
         ax2.legend(loc="lower right")
         plt.show()
+
+
